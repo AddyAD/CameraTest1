ce584f2b7f16c2a4d64742e8efa320a2fe936442
ad <ad@itmp.top>
2015-10-02 12:22:07 +0800

    ui update & header added

--- app/src/main/java/com/example/hello/cameratest1/MainActivity.java
+++ app/src/main/java/com/example/hello/cameratest1/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.hello.cameratest1;
 
+import java.io.File;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Environment;
 public class MainActivity extends Activity {
 
     @Override
