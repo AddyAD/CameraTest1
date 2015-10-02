50f10e4cdd08b70ff63355e2b5b0d48ea4f2b100
ad <ad@itmp.top>
2015-10-02 12:54:40 +0800

    done with error

--- app/src/main/java/com/example/hello/cameratest1/MainActivity.java
+++ app/src/main/java/com/example/hello/cameratest1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.hello.cameratest1;
 
 import java.io.File;
 import android.app.Activity;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +15,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.Environment;
+
+
 public class MainActivity extends Activity {
     private Button dissubmit=null;
     private Button submit=null;
@@ -38,10 +42,30 @@ public class MainActivity extends Activity {
         submit.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED) ){
+                    Toast.makeText(getApplicationContext(),"读取失败,SD存储卡不存在!",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                path = Environment.getExternalStorageState().toString()+File.separator+
+                        "hehe"+File.separator+FN;
+                message=new File(path);
+                /*if(!message.exists()){
+                    File vpath=message.getParentFile();
+                    vpath.mkdir();
+                    Toast.makeText(getApplicationContext(),"文件不存在!!",Toast.LENGTH_LONG).show();
+                    return;
+                }*/
+                Uri uri=Uri.fromFile(message);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+                startActivityForResult(intent,1);
+
 
             }
         });
     }
 
 
+
+    }
 }
